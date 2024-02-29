9780b5a19adf314af3996db74e11bfcba94441c7
Fatih Asci <126919157+mrfatihasci@users.noreply.github.com>
2024-02-29 05:33:55 +0300

    remove event listeners when width is 768px +

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ document.addEventListener("readystatechange", event => {
 });
 const initApp = () => {
   setInterval(changeBanner, 10000);
-  typeWriterInterval();
+  typeWriterInterval = setInterval(typeWriter2, 150);
   document.getElementById("footer__year").innerHTML = new Date().getFullYear(); //prettier-ignore
   navMenuCollapsing();
-  footerUlCollapsing();
+  footerMenu.init();
 };
 
 // BANNER ----------------------------------------------------------------
@@ -27,6 +27,7 @@ const changeBanner = () => {
   i += 1;
   something();
 };
+
 function something() {
   const newspaperSpinning = [
     { transform: "translateY(25px)" },
@@ -42,39 +43,47 @@ function something() {
 
 //type writing code--------------------------------------------------------------
 const typewritingP = document.querySelector("#p--typewriting");
-const text =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero iste perferendis architecto eos eligendi placeat mollitia vitae dolores qui!";
+const text ="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero iste perferendis architecto eos eligendi placeat mollitia vitae dolores qui!";
 const textArray = text.split(" ");
 const textArrayLength = textArray.length;
 let x = 0;
-var typeWriterInterval = () => {
-  setInterval(typeWriter, 150);
-};
-function typeWriter2() {
-  // Typewriter effect function for the text of element to be added character by character
+let typeWriterInterval;
+  // Add typewriter effect function for the text of element to be added character by character(2letters at once)
+function typeWriter() {
   if (x < text.length) {
     typewritingP.innerHTML += text.charAt(x);
     x++;
-    //    console.log("setInterval is Active!");
   } else {
     clearInterval(typeWriterInterval);
-    console.log("setInterval is inActive!");
+    console.log("typewriter setInterval is inActive!");
   }
 }
-function typeWriter() {
-  //Typewritter effect function for the text of element to be added word by word
-  if (x <= textArrayLength - 1) {
-    typewritingP.innerText += ( " " + textArray[x] ); // prettier-ignore
+// Add typewritter effect function for the text of element to be added word by word
+function typeWriter2() {
+  if (x < textArrayLength) {
+    typewritingP.innerHTML += ( " " + textArray[x] ); // prettier-ignore
     x++;
-    //    console.log("setInterval is Active!");
   } else {
     clearInterval(typeWriterInterval);
-    console.log("setInterval is inActive!");
+    console.log("typewriter setInterval is inActive!");
   }
 }
 
-//nav-menu event listener------------------------------------------------------------
+// Add event listener for nav menu------------------------------------------------------------
 const navMenuCollapsing = () => {
+  const menuButton = document.querySelector("#menu-button");
+  const menu = document.querySelector("#nav-ul");
+
+  const toggleMenu = () => {
+    if (menu.classList.contains("menu-open")) {
+      menu.classList.remove("menu-open");
+    } else {
+      menu.classList.add("menu-open");
+    }
+  };
+  menuButton.addEventListener("click", toggleMenu);
+};
+/* const navMenuCollapsing = () => {
   const menuButton = document.querySelector("#menu-button");
   const menu = document.querySelector("#nav-ul");
   menuButton.addEventListener("click", () => {
@@ -84,60 +93,118 @@ const navMenuCollapsing = () => {
       menu.classList.add("menu-open");
     }
   });
-};
+}; */
 
-//footer unorder list collapsing events------------------------------------------------
-const footerUlCollapsing = () => {
-  const helpMenu = document.getElementById("footer__help");
-  const aboutMenu = document.getElementById("footer__about");
-  const socialMenu = document.getElementById("footer__social");
-  const otherMenu = document.getElementById("footer__other");
-  const mediaQuery = window.matchMedia("(min-width: 768px)");
-  if (mediaQuery.matches) {
-    helpMenu.classList.remove("offscreen");
-    aboutMenu.classList.remove("offscreen");
-    socialMenu.classList.remove("offscreen");
-    otherMenu.classList.remove("offscreen");
-  } else {
-    const helpButton = document.getElementById("footer__help-button");
-    helpButton.addEventListener("click", event => {
-      if (helpButton.classList.contains("blue")) {
-        helpButton.classList.remove("blue");
-        helpMenu.classList.add("offscreen");
-      } else {
-        helpButton.classList.add("blue");
-        helpMenu.classList.remove("offscreen");
-      }
-    });
-    const aboutButton = document.getElementById("footer__about-button");
-    aboutButton.addEventListener("click", event => {
-      if (aboutButton.classList.contains("blue")) {
-        aboutButton.classList.remove("blue");
-        aboutMenu.classList.add("offscreen");
-      } else {
-        aboutButton.classList.add("blue");
-        aboutMenu.classList.remove("offscreen");
-      }
-    });
-    const socialButton = document.getElementById("footer__social-button");
-    socialButton.addEventListener("click", event => {
-      if (socialButton.classList.contains("blue")) {
-        socialButton.classList.remove("blue");
-        socialMenu.classList.add("offscreen");
-      } else {
-        socialButton.classList.add("blue");
-        socialMenu.classList.remove("offscreen");
-      }
-    });
-    const otherButton = document.getElementById("footer__other-button");
-    otherButton.addEventListener("click", event => {
-      if (otherButton.classList.contains("blue")) {
-        otherButton.classList.remove("blue");
-        otherMenu.classList.add("offscreen");
-      } else {
-        otherButton.classList.add("blue");
-        otherMenu.classList.remove("offscreen");
-      }
-    });
+// Add footer menu event listeners  -------------------------------------------------------
+const footerMenu = {
+  helpMenu: null,
+  aboutMenu: null,
+  socialMenu: null,
+  otherMenu: null,
+  mediaQuery: null,
+  isScreenWidthLarge: false,
+  helpButton: null,
+  aboutButton: null,
+  socialButton: null,
+  otherButton: null,
+
+  init() {
+    this.helpMenu = document.getElementById("footer__help");
+    this.aboutMenu = document.getElementById("footer__about");
+    this.socialMenu = document.getElementById("footer__social");
+    this.otherMenu = document.getElementById("footer__other");
+    this.mediaQuery = window.matchMedia("(min-width: 768px)");
+    this.isScreenWidthLarge = this.mediaQuery.matches;
+    this.helpButton = document.getElementById("footer__help-button");
+    this.aboutButton = document.getElementById("footer__about-button");
+    this.socialButton = document.getElementById("footer__social-button");
+    this.otherButton = document.getElementById("footer__other-button");
+
+    // Bind event listeners
+    this.helpEvent = this.helpEvent.bind(this);
+    this.aboutEvent = this.aboutEvent.bind(this);
+    this.socialEvent = this.socialEvent.bind(this);
+    this.otherEvent = this.otherEvent.bind(this);
+    this.addRemoveEventListeners = this.addRemoveEventListeners.bind(this);
+
+    // Add initial event listeners and set initial state
+    this.addRemoveEventListeners(this.isScreenWidthLarge);
+    this.updateMenus();
+
+    // Add resize event listener
+    window.addEventListener("resize", () => this.handleResize());
+  },
+
+  handleResize() {
+  const newIsScreenWidthLarge = this.mediaQuery.matches;
+  
+  if (newIsScreenWidthLarge !== this.isScreenWidthLarge) {
+    this.isScreenWidthLarge = newIsScreenWidthLarge;
+    this.updateMenus();
+    this.addRemoveEventListeners(this.isScreenWidthLarge);
   }
+},
+
+  updateMenus() {
+    if (this.isScreenWidthLarge) {
+      this.showMenus();
+    } else {
+      this.hideMenus();
+    }
+  },
+
+  showMenus() {
+    this.helpMenu.classList.remove("offscreen");
+    this.aboutMenu.classList.remove("offscreen");
+    this.socialMenu.classList.remove("offscreen");
+    this.otherMenu.classList.remove("offscreen");
+    this.helpButton.classList.remove("blue");
+    this.aboutButton.classList.remove("blue");
+    this.socialButton.classList.remove("blue");
+    this.otherButton.classList.remove("blue");
+  },
+
+  hideMenus() {
+    this.helpMenu.classList.add("offscreen");
+    this.aboutMenu.classList.add("offscreen");
+    this.socialMenu.classList.add("offscreen");
+    this.otherMenu.classList.add("offscreen");
+  },
+
+  helpEvent() {
+    this.toggleButtonAndMenu(this.helpButton, this.helpMenu);
+  },
+
+  aboutEvent() {
+    this.toggleButtonAndMenu(this.aboutButton, this.aboutMenu);
+  },
+
+  socialEvent() {
+    this.toggleButtonAndMenu(this.socialButton, this.socialMenu);
+  },
+
+  otherEvent() {
+    this.toggleButtonAndMenu(this.otherButton, this.otherMenu);
+  },
+
+  toggleButtonAndMenu(button, menu) {
+    button.classList.toggle("blue");
+    menu.classList.toggle("offscreen");
+  },
+
+  addRemoveEventListeners(addListeners) {
+    if (!addListeners) {
+      this.helpButton.addEventListener("click", this.helpEvent);
+      this.aboutButton.addEventListener("click", this.aboutEvent);
+      this.socialButton.addEventListener("click", this.socialEvent);
+      this.otherButton.addEventListener("click", this.otherEvent);
+      console.log("footer-menu event listeners are active");
+    } else {
+      this.helpButton.removeEventListener("click", this.helpEvent);
+      this.aboutButton.removeEventListener("click", this.aboutEvent);
+      this.socialButton.removeEventListener("click", this.socialEvent);
+      this.otherButton.removeEventListener("click", this.otherEvent);
+      console.log("footer-menu event listeners are inActive");
+    }
+  },
 };
