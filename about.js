1ec5ecfbef7a08528bca7430926095af8caa1245
Fatih Asci <126919157+mrfatihasci@users.noreply.github.com>
2024-03-09 04:05:50 +0300

    edit readme, change banner animation, split js

--- about.js
+++ about.js
@@ -5,8 +5,7 @@ document.addEventListener("readystatechange", event => {
   }
 });
 const initApp = () => {
-  setInterval(changeBanner, 3000);
-  typeWriterInterval = setInterval(typeWriter2, 150);
+  setInterval(changeBanner, 10000);
   document.getElementById("footer__year").innerHTML = new Date().getFullYear(); //prettier-ignore
   navMenuCollapsing();
   footerMenu.init();
@@ -49,28 +48,6 @@ const changeBanner = () => {
     i += 1;
   };
 };
-
-//type writing code--------------------------------------------------------------
-const typewritingP = document.querySelector("#p--typewriting");
-const text =
-  "setInterval for this typewriting effect is removed when completed, 5 event listeners for menuButton and footer list items are removed with only one left, the window resize event listener(in branch 'tailwindcss' only), which is basically used to add those removed event listeners when necessary. I'm not sure and did not compare the two cases if they were really necessary to remove. well we can discuss..";
-const textArray = text.split(" ");
-const textArrayLength = textArray.length;
-let x = 0;
-let typeWriterInterval;
-// Add typewriter effect function for the text of element to be added character by character(2letters at once)
-// depreceated & deleted
-// Add typewritter effect function for the text of element to be added word by word
-function typeWriter2() {
-  if (x < textArrayLength) {
-    typewritingP.innerHTML =  textArray[x] ; // prettier-ignore
-    x++;
-  } else {
-    clearInterval(typeWriterInterval);
-    console.log("typewriter setInterval is inActive!");
-    typewritingP.innerHTML = text;
-  }
-}
 
 // Add event listener for nav menu------------------------------------------------------------
 const navMenuCollapsing = () => {
